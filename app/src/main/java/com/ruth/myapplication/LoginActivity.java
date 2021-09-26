b81ac7e60de2f642fa36448a935d38dcef0f79a3
v-mari <violettrujillo565@gmail.com>
2021-09-26 09:22:27 -0400

    CoordinatorLayout y TextImputText

---
+++ app/src/main/java/com/ruth/myapplication/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.ruth.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
