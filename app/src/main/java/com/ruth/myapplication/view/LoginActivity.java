802928ba7d086f423394aaf7f8745fe7caa746f0
V-mari <82954569+V-mari@users.noreply.github.com>
2021-09-26 20:05:38 -0400

    CardView

--- app/src/main/java/com/ruth/myapplication/view/LoginActivity.java
+++ app/src/main/java/com/ruth/myapplication/view/LoginActivity.java
@@ -1,23 +1,30 @@
-package com.ruth.myapplication;
+package com.ruth.myapplication.view.;
 
+import androidx.appcompat.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.ruth.myapplication.view.ContainerActivity;
+import com.ruth.myapplication.view.LoginActivity;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activityLogin);
     }
+
     public void goCreateAccount(View view){
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
     }
 
     public void login(View view) {
+        Intent intent = new Intent(this, containerActivity.class);
+        startActivity(intent);
     }
 }
